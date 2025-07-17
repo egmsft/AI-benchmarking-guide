a0d752d42923f494eb4b4f05b2a9cdfb3d109182
gitaumark <156354973+gitaumark@users.noreply.github.com>
2025-07-17 11:43:06 -0400

    remove config redundancy in NVBandwidth.py

--- Benchmarks/NVIDIA/NVBandwidth.py
+++ Benchmarks/NVIDIA/NVBandwidth.py
@@ -7,24 +7,6 @@ class NVBandwidth:
     def __init__(self, path:str, machine: str):
         self.name='NVBandwidth'
         self.machine_name = machine
-        config = self.get_config(path)
-        self.num_runs, self.interval = self.config_conversion(config)
-        self.buffer = []
-
-    def get_config(self, path: str):
-        file = open(path)
-        data = json.load(file)
-        file.close()
-        try:
-            return data[self.name]
-        except KeyError:
-            raise KeyError("no value found")
-
-    def parse_json(self, config):
-        return config['inputs']['num_runs'], config['inputs']['interval']
-
-    def config_conversion(self, config)->tuple[list, list, list]:
-        return self.parse_json(config)
 
     def build(self):
         current = os.getcwd()
@@ -47,17 +29,13 @@ class NVBandwidth:
         current = os.getcwd()
         os.chdir(os.path.join(current, 'nvbandwidth'))
         print("Running NVBandwidth...")
-        buffer=[]
         results = subprocess.run('./nvbandwidth -t device_to_host_memcpy_ce host_to_device_memcpy_ce device_to_device_bidirectional_memcpy_read_ce', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         tools.write_log(tools.check_error(results))
         log = results.stdout.decode('utf-8')
-        buffer.append(log)
         os.chdir(current)
 
         file = open("Outputs/NVBandwidth_" + self.machine_name + ".txt", "w")
-        for item in buffer:
-            file.write(item)
-            print(item)
-
-        self.buffer=buffer
+        item = "\n".join(log[log.find("device_to_host_memcpy_ce"):].strip().splitlines()[:-2])
+        file.write(item)
+        print(item)
         os.chdir(current)
