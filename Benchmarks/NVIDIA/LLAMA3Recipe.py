4ff38dfb96d5f79dc44c6d133d45890739cd7542
Mishty <t-mdhekial@microsoft.com>
2025-08-14 17:20:50 -0700

    Pushing after confirming test works on H200

--- Benchmarks/NVIDIA/LLAMA3Recipe.py
+++ Benchmarks/NVIDIA/LLAMA3Recipe.py
@@ -77,7 +77,7 @@ def configure_recipe(cfg, nodes=1, gpus_per_node=4):
     if model_size == "3b":
         recipe.trainer.max_time = "0:02:00:00" # stop after 2 hours
     else:
-        recipe.trainer.max_time = "0:00:15:00" # stop after 4 hours
+        recipe.trainer.max_time = "0:04:00:00" # stop after 4 hours
     return recipe
 
 
