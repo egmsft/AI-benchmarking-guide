56420009b76762c435451e0aca30080f4c751e5b
gitaumark <156354973+gitaumark@users.noreply.github.com>
2025-08-25 14:36:22 -0400

    Update TensorRT-LLM version to 0.18.2

--- Benchmarks/NVIDIA/LLMBenchmark.py
+++ Benchmarks/NVIDIA/LLMBenchmark.py
@@ -33,13 +33,13 @@ class LLMBenchmark:
         # Clone TensorRT-LLM repo
         if not os.path.exists(os.path.join(self.dir_path, 'TensorRT-LLM')):
             print("Cloning TensorRT-LLM reopsitory from https://github.com/NVIDIA/TensorRT-LLM.git")
-            i4 = subprocess.run("git clone https://github.com/NVIDIA/TensorRT-LLM.git", shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            i4 = subprocess.run("git clone https://github.com/NVIDIA/TensorRT-LLM.git && cd TensorRT-LLM && git checkout v0.18.2", shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
             tools.write_log(tools.check_error(i4))
 
             if not os.path.exists("/.dockerenv"):
                 # Install required packages
                 print("No Docker container detected. Installing tensorrt-llm")
-                i2 = subprocess.run("pip install tensorrt-llm==0.20.0", shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+                i2 = subprocess.run("pip install tensorrt-llm==0.18.2", shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
                 tools.write_log(tools.check_error(i2))
                 i2 = subprocess.run("sudo apt update && sudo apt-get -y install libopenmpi-dev", shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
                 tools.write_log(tools.check_error(i2))
